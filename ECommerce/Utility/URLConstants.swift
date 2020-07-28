d38db9f7d13f7a7372aceb8e254cbdca6468d205
Nishita Shukla <shuklanishita@gmail.com>
2020-07-28 23:44:19 +0530

    URL Constants File

---
+++ ECommerce/Utility/URLConstants.swift
@@ -0,0 +1,14 @@
+//
+//  URLConstants.swift
+//  ECommerce
+//
+//  Created by Nishita Shukla on 28/07/20.
+//  Copyright © 2020 Nishita Shukla. All rights reserved.
+//
+
+import Foundation
+
+struct URLConstants
+{
+   static let BASEURL = "https://stark-spire-93433.herokuapp.com/json"
+}
